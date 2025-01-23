2845146cc702829ed0b2273d5c4a2606f8961d81
Yuri Santiago <yurisantiagx5@gmail.com>
2025-01-23 20:50:50 -0300

    update

--- HabitTracker/HabitTrackerUITests/HabitTrackerUITests.swift
+++ HabitTracker/HabitTrackerUITests/HabitTrackerUITests.swift
@@ -3,7 +3,6 @@ import XCTest
 final class HabitTrackerUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
